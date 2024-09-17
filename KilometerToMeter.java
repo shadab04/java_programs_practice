a2bda4dc21f34fe3dfb95139eccc5398af5f2760
shadab khan <your-email@example.com>
2024-09-17 21:31:37 +0530

    added new programs

---
+++ KilometerToMeter.java
@@ -0,0 +1,10 @@
+package java_programs_practice;
+
+public class KilometerToMeter {
+    public static void main(String[] args) {
+        int M1=30;
+        int convert=M1*1000;
+        System.out.print("conver 30 kilometer to meter:");
+        System.out.println(convert);
+    }
+}
---
+++ Sum_nums.java
@@ -0,0 +1,11 @@
+package java_programs_practice;
+public class Sum_nums {
+    public static void main(String[] args) {
+        int n1=20;
+        int n2=90;
+        int n3=20;
+        int sum=0;
+        sum=n1+n2+n3;
+        System.out.println(sum);
+    }
+}
